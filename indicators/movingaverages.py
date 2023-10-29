6a311eac54861e7447d98264cccaf0973731c99b
azak <azak@protonmail.com>
2023-10-29 22:34:53 +0100

    Minor style changes

--- indicators/movingaverages.py
+++ indicators/movingaverages.py
@@ -5,26 +5,25 @@ import backtrader as bt
 
 # Create a Stratey
 class EMAStrategy(bt.Strategy):
-    params = (('period', 20),)
+    params = (("period", 20),)
 
     def __init__(self):
         self.sma = bt.talib.SMA(self.data, timeperiod=self.p.period)
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
-                # To keep track of pending orders and buy price/commission
+        # To keep track of pending orders and buy price/commission
         self.order = None
         self.buyprice = None
         self.buycomm = None
 
-    
     def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
+        """Logging function fot this strategy"""
         dt = dt or self.datas[0].datetime.date(0)
-        print(dt.isoformat() +" , " + txt)
-    
+        print(dt.isoformat() + " , " + txt)
+
     def next(self):
         # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
+        self.log("Close, %.2f" % self.dataclose[0])
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
@@ -32,26 +31,24 @@ class EMAStrategy(bt.Strategy):
 
         # Check if we are in the market
         if not self.position:
-
             # Not yet ... we MIGHT BUY if ...
             if self.dataclose[0] < self.dataclose[-1]:
-                    # current close less than previous close
+                # current close less than previous close
 
-                    if self.dataclose[-1] < self.dataclose[-2]:
-                        # previous close less than the previous close
+                if self.dataclose[-1] < self.dataclose[-2]:
+                    # previous close less than the previous close
 
-                        # BUY, BUY, BUY!!! (with default parameters)
-                        self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                    # BUY, BUY, BUY!!! (with default parameters)
+                    self.log("BUY CREATE, %.2f" % self.dataclose[0])
 
-                        # Keep track of the created order to avoid a 2nd order
-                        self.order = self.buy()
+                    # Keep track of the created order to avoid a 2nd order
+                    self.order = self.buy()
 
         else:
-
             # Already in the market ... we might sell
             if len(self) >= (self.bar_executed + 5):
                 # SELL, SELL, SELL!!! (with all possible default parameters)
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+                self.log("SELL CREATE, %.2f" % self.dataclose[0])
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
@@ -65,14 +62,14 @@ class EMAStrategy(bt.Strategy):
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+                self.log("BUY EXECUTED, %.2f" % order.executed.price)
             elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+                self.log("SELL EXECUTED, %.2f" % order.executed.price)
 
             self.bar_executed = len(self)
 
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
+            self.log("Order Canceled/Margin/Rejected")
 
         # Write down: no pending order
         self.order = None
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import yfinance as yf
 from indicators import movingaverages
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cerebro = bt.Cerebro()
 
     cerebro.addstrategy(movingaverages.EMAStrategy)
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
 
-    data = bt.feeds.PandasData(dataname=yf.download('TSLA', '2018-01-01', '2019-01-01'))
+    data = bt.feeds.PandasData(dataname=yf.download("TSLA", "2018-01-01", "2019-01-01"))
 
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
@@ -22,14 +22,14 @@ if __name__ == '__main__':
     # Set our desired cash start
     cerebro.broker.setcash(100000.0)
 
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print("Starting Portfolio Value: %.2f" % cerebro.broker.getvalue())
 
     cerebro.run()
 
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
 
     # Plot the result
     cerebro.plot()
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    sys.exit(main.main(sys.argv[1:]))
