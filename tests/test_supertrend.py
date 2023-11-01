412a5585e973047660fe8a266c133563fc204f3b
azak <azak@protonmail.com>
2023-11-01 17:44:43 +0100

    Adds Test for supertrend with backtrader

--- tests/test_supertrend.py
+++ tests/test_supertrend.py
@@ -1,5 +1,6 @@
 import http.client
 import yfinance as yf
+import backtrader as bt
 import os
 from indicators import supertrend
 from datetime import datetime, timedelta
@@ -26,21 +27,19 @@ def test_backtest_supertrend():
     atr_period = 7
     atr_multiplier = 3
 
-    stock_list = ["AAPL"]
+    stock_list = ["TSLA"]
     for symbol in stock_list:
         date = datetime.now() - timedelta(days=1)
         start_date = date - timedelta(days=5)
 
         df = yf.download(
             symbol,
-            start=start_date.strftime("%Y-%m-%d"),
-            end=date.strftime("%Y-%m-%d"),
-            interval="5m",
+            start=start_date.strftime("2020-01-01"),
+            end=date.strftime("2022-01-01"),
         )
         st = supertrend.supertrend(df, atr_period, atr_multiplier)
         df = df.join(st)
 
-    # df.tail(15)
     entry, exit, roi = supertrend.backtest_supertrend(df, 10000, debug=True)
     assert len(entry) != 0
     assert len(exit) != 0
@@ -55,3 +54,15 @@ def test_find_optimal_parameter():
     print(
         f"Best parameter set: ATR Period={optimal_param[0]}, Multiplier={optimal_param[1]}, ROI={optimal_param[2]}"
     )
+
+
+def test_supertrend_backtrader():
+    cerebro = bt.Cerebro()
+    cerebro.addstrategy(supertrend.SuperTrendStrategy)
+    data = bt.feeds.PandasData(dataname=yf.download("TSLA", "2020-01-01", "2022-01-01"))
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+    # Set our desired cash start
+    cerebro.broker.setcash(10000.0)
+    cerebro.run()
+    #cerebro.plot()
